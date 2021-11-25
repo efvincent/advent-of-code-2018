3c3f157bcce78f420d8e65e2585fdd6e61950e12
Eric Vincent <eric@efvincent.com>
2021-11-25 11:44:42 -0500

    cleanup day 1 & 2

--- src/AOC/Challenge/Day01.hs
+++ src/AOC/Challenge/Day01.hs
@@ -10,54 +10,45 @@
 --
 -- Day 1.  See "AOC.Solver" for the types used in this module!
 --
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day01 (
     day01a
   , day01b
   ) where
 
-import AOC.Solver
+import AOC.Solver (  (:~>)(..) )
 import Data.IntSet (fromList, member, insert, singleton)
 
-dep :: String -> Int
-dep = read . \case {
+-- | @read@ function doesn't parse the plus in "+10". Remove the
+-- leading plus if it's the first character in the string
+unplus :: String -> Int
+unplus = read . \case {
     '+':rest -> rest;
     x -> x
   }
 
-p2 :: [Int] -> Int
-p2 nums =
+-- | use @cycle@ to create an infinite list that loops from the input list,
+-- accumulating the result of adding the next number in the list in an 
+-- @IntSet@ until we see one that has been seen before.
+solve1b :: [Int] -> Int
+solve1b nums =
   go (singleton 0) 0 (cycle nums)
   where
-    go seen cur xs =
-      if member y seen 
-        then y 
-        else go (insert y seen) y rest
-      where
-        y = head xs + cur
-        rest = tail xs
-
+    go seen cur (x:xs) 
+      | member (x+cur) seen = x+cur
+      | otherwise = go (insert (x+cur) seen) (x+cur) xs
+    go _ _ [] = error "We'll never see this; cycle produces infinite list"
 
 day01a :: [Int] :~> Int
 day01a = MkSol
-    { sParse = Just . map dep . lines
+    { sParse = Just . map unplus . lines
     , sShow  = show
     , sSolve = Just . sum
     }
 
 day01b :: [Int] :~> Int
 day01b = MkSol
-    { sParse = Just . map dep . lines
+    { sParse = Just . map unplus . lines
     , sShow  = show
-    , sSolve = Just . p2 
+    , sSolve = Just . solve1b 
     }
